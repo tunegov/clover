cbeab3a8efda0f0075daad9af34d9c3c552afd9b
Ivan Ivaschenko <iivaschenko@zaraffasoft.com>
2018-10-14 13:39:03 +0300

    added i18n for multinalguages

--- android/app/src/main/java/com/clovercompany/MainApplication.java
+++ android/app/src/main/java/com/clovercompany/MainApplication.java
@@ -3,6 +3,7 @@ package com.clovercompany;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import it.innove.BleManagerPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNI18nPackage(),
             new MapsPackage(),
             new LinearGradientPackage(),
             new BleManagerPackage(),
